939fb72ae843b3943b8b57d8917ac65613e9e338
BurakParsAydin <hi@burakaydin.net>
2025-07-05 23:29:15 +0300

    Continue implementing HPOS export handler logic

--- plugins/woocommerce/src/Internal/Admin/ImportExport/HposOrderExportHandler.php
+++ plugins/woocommerce/src/Internal/Admin/ImportExport/HposOrderExportHandler.php
@@ -1,16 +1,18 @@
 <?php
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\Internal\Admin\ImportExport;
+
+use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController;
+
+defined( 'ABSPATH' ) || exit;
+
 /**
  * Handles the export of HPOS (High-Performance Order Storage) orders
  * via the WordPress Tools > Export functionality.
  *
  * @since {$version}
  */
-
-namespace Automattic\WooCommerce\Internal\Admin\ImportExport;
-
-defined( 'ABSPATH' ) || exit;
-
-
 class HposOrderExportHandler {
 
 	/**
@@ -24,6 +26,7 @@ class HposOrderExportHandler {
 	 * Constructor. Registers the export hook.
 	 */
 	public function __construct() {
+		add_action( 'export_wp', array( $this, 'maybe_output_hpos_orders' ), 10, 1 );
 	}
 
 	/**
@@ -36,5 +39,50 @@ class HposOrderExportHandler {
 			self::$instance = new self();
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Hook into the WordPress export system and output HPOS orders if applicable.
+	 *
+	 * @param array $args Export arguments.
+	 */
+	public function maybe_output_hpos_orders( $args ) {
+		if ( ! isset( $args['content'] ) || 'shop_order' !== $args['content'] ) {
+			return;
+		}
+
+		$controller = wc_get_container()->get( CustomOrdersTableController::class );
+
+		// Only run this if HPOS is enabled.
+		if ( ! $controller->custom_orders_table_usage_is_enabled() ) {
+			return;
+		}
+
+		$orders = wc_get_orders(
+			array(
+				'limit'        => -1,
+				'return'       => 'ids',
+				'orderby'      => 'date_created',
+				'order'        => 'ASC',
+				'type'         => 'shop_order',
+				'paginate'     => false,
+				'date_created' => '>' . gmdate( 'Y-m-d H:i:s', 0 ),
+			)
+		);
+
+		foreach ( $orders as $order_id ) {
+			$order = wc_get_order( $order_id );
+			if ( $order ) {
+				$this->export_order_to_xml( $order );
+			}
+		}
+	}
+
+	/**
+	 * Outputs orders in XML format.
+	 *
+	 * @param \WC_Order $order The WooCommerce order object.
+	 */
+	protected function export_order_to_xml( $order ) {
 	}
 }
